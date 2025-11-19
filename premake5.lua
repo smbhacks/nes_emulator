8596c51c8fe95aa0797f154b104bd16a12aca6d3
studsX <zipzipchess@gmail.com>
2025-11-19 19:08:43 +0100

    Revert "test"
    This reverts commit d971dd54f888f69d1d236ecee626851a260ceea6.

--- premake5.lua
+++ premake5.lua
@@ -8,67 +8,20 @@ project "NES_Emulator"
     targetdir "bin/%{cfg.buildcfg}"
     files { "src/**.c", "src/**.h" }
 
-    files {
-        "lib/gl3w/gl3w.c"
-    }
-    includedirs {
-        "lib/gl3w"
-    }
-
-    ---------------------------------------
-    -- cimgui + imgui
-    ---------------------------------------
-    files {
-        "lib/cimgui/cimgui.cpp",
-        "lib/cimgui/imgui/*.cpp",
---        "lib/cimgui/imgui/backends/imgui_impl_glfw.cpp",
-        "lib/cimgui/imgui/backends/imgui_impl_opengl3.cpp",
-        "lib/cimgui/imgui/backends/imgui_impl_sdl2.cpp"
-    }
-
-    includedirs {
-        "lib/cimgui",
-        "lib/cimgui/imgui",
-        "lib/cimgui/imgui/backends"
-    }
-
-    filter {
-        "files:lib/cimgui/**.cpp",
-        "files:lib/cimgui/imgui/**.cpp",
-        "files:lib/cimgui/imgui/backends/**.cpp"
-    }
-        language "C++"
-    filter {}
-
-    ---------------------------------------
-    -- SDL2
-    ---------------------------------------
+    -- Header files
     includedirs { "lib/SDL2/include/SDL2" }
+
+    -- Library folder
     libdirs { "lib/SDL2/lib" }
 
-
-    ---------------------------------------
-    -- gmake (Linux build)
-    ---------------------------------------
     filter "action:gmake*"
         buildoptions { "-Wall", "-Werror" }
-        links { "SDL2" }
+	links { "SDL2" }
 
-        -- IMPORTANT: link with the C++ runtime
-        linkoptions { "-lstdc++" }
-
-
-    ---------------------------------------
-    -- Windows (VS2022)
-    ---------------------------------------
     filter "action:vs*"
         defines { "_CRT_SECURE_NO_WARNINGS" }
         buildoptions { "/W3", "/WX" }
-        links { "SDL2-static.lib", "SDL2main.lib" }
-
-        -- Required for linking C++ objects into a C project
-        links { "msvcprt.lib", "msvcp140.lib" }
-
+	links { "SDL2-static.lib", "SDL2main.lib" }
 
     filter "system:windows"
         defines { "SDL_STATIC", "SDL_MAIN_HANDLED" }
@@ -91,6 +44,5 @@ project "NES_Emulator"
 
     filter "configurations:Debug"
         symbols "On"
-
     filter "configurations:Release"
         optimize "On"
